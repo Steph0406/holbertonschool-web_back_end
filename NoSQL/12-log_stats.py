08bbe1765814d10c88e2abe422770d00c961de66
Steph0406 <carobayo04@gmail.com>
2026-04-22 23:15:19 -0600

    Refactor log_stats.py to improve variable naming

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,10 +6,17 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient()
-    col = client.logs.nginx
+    collection = client.logs.nginx
 
-    print("{} logs".format(col.count_documents({})))
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("\tmethod {}: {}".format(method, col.count_documents({"method": method})))
-    print("{} status check".format(col.count_documents({"method": "GET", "path": "/status"})))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(count))
